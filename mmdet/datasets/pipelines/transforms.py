67fe92db94412d53c06c78ebaffda2b8c9c3015b
HT-hlf <htbao123456@163.com>
2022-04-18 20:33:44 +0800

    ht7

--- mmdet/datasets/pipelines/transforms.py
+++ mmdet/datasets/pipelines/transforms.py
@@ -1352,17 +1352,17 @@ class PhotoMetricDistortion_rgb_depth:
         depth=img[:, :, 3:]
         # if img_filename.split('_')[0]!='rgb':
         # rand_count=random.randint(4)
-        if random.randint(10):
-            delta = random.uniform(-55, 55)
-            rgb += delta
-        else:
-            if img_filename.split('_')[0] == 'bg':
+        if img_filename.split('_')[0] == 'bg':
+            if random.randint(3):
+                delta = random.uniform(-55, 55)
+                rgb += delta
+            else:
                 depth = np.zeros_like(depth, dtype=np.float32)
                 delta = random.uniform(-5, 5)
                 rgb += delta
-            else:
-                delta = random.uniform(-15, 15)
-                rgb += delta
+        else:
+            delta = random.uniform(-15, 15)
+            rgb += delta
 
         # no_b_path=r'G:\mmdetection_miner\PhotoMetricDistortion_rgb_depth_test\no_bright'+'/'+img_filename
 
